c495277f89e4b579fc7caed6863b1108d9970d62
Tatomchin <tom.kowprasert@gmail.com>
2023-04-29 02:19:27 +0700

    Update Check El Btn Nav

--- tk-webport/src/App.js
+++ tk-webport/src/App.js
@@ -7,49 +7,105 @@ import Sayhi from './components/Sayhi';
 import Skills from './components/Skills/Skills';
 import UnderBar from './components/UnderBar/UnderBar';
 
-import { useEffect, useRef } from 'react';
-
-
-
+import React,{ useEffect, useRef, useState } from 'react';
+const ConData = React.createContext();
 function App() {
+  // const [techinTag,setTechinTag] = useState(null);
+  const skillTag = useRef(null);
+  const contactTag = useRef(null);
+  const projectTag = useRef(null);
 
+  const [st_SkillTagTop, setST_SkillTagTop] = useState(false);
+  const [st_ContactTagTop, setST_ContactTagTop] = useState(false);
+  const [st_ProjectTagTop, setST_ProjectTagTop] = useState(false);
 
-  const techinTag = useRef(null);
-  const options = {
-    threshold: 1
-  }
+  const scrollHandler = () => {
+    let skillTagTop = skillTag.current.getBoundingClientRect();
+    let contactTagTop = contactTag.current.getBoundingClientRect();
+    let projectTagTop = projectTag.current.getBoundingClientRect();
+
+    if (skillTagTop.top <= 10) {
+      setST_SkillTagTop(true)
+      setST_ContactTagTop(false)
+      setST_ProjectTagTop(false)
+    } else { setST_SkillTagTop(false) }
+
+    if (contactTagTop.top <= 10) {
+      setST_SkillTagTop(false)
+      setST_ContactTagTop(true)
+      setST_ProjectTagTop(false)
+    } else { setST_ContactTagTop(false) }
+
+    if (projectTagTop.top <= 10) {
+      setST_SkillTagTop(false)
+      setST_ContactTagTop(false)
+      setST_ProjectTagTop(true)
+    } else { setST_ProjectTagTop(false) }
+  };
 
   useEffect(() => {
-
-    const observerKub = new IntersectionObserver(wait => {
-      console.log(wait)
-    }, {options});
-    if (techinTag.current) {
-      observerKub.observe(techinTag.current)
-    }
+    window.addEventListener("scroll", scrollHandler, true);
     return () => {
-      if (techinTag.current) {
-        observerKub.unobserve(techinTag.current)
-      }
-    }
-    }, [techinTag, options])
+      window.removeEventListener("scroll", scrollHandler, true);
+    };
+  }, []);
+  // console.log('skill = ' + st_SkillTagTop)
+  // console.log('contact = ' + st_ContactTagTop)
+  // console.log('project = ' + st_ProjectTagTop)
+
+  // useEffect(() => {
+  //   let observerRefValue = null;
+  //   const observerKub = new IntersectionObserver((items) => {
+  //     console.log(items[0]);
+  //   }, { rootMargin: "0px" });
+  //   if (skillTag.current) {
+  //     observerKub.observe(skillTag.current)
+  //     observerRefValue = skillTag.current;
+  //   }
+  //   return () => {
+  //     if (observerRefValue) {
+  //     observerKub.unobserve(observerRefValue)
+  //   }}
+  // }, [skillTag])
+
+  // useEffect(() => {
+  //   let observerRefValue = null;
+  //   let options = {
+  //     root: null,
+  //     rootMargin: "0px",
+  //     threshold: 0.25
+  //   };
+  //   const observerKub = new IntersectionObserver((items) => {
+  //     // console.log(items);
+  //   }, { options });
+  //   if (contactTag.current) {
+  //     observerKub.observe(contactTag.current)
+  //     observerRefValue = contactTag.current;
+  //   }
+  //   return () => {
+  //     if (observerRefValue) {
+  //     observerKub.unobserve(observerRefValue)
+  //   }}
+  // }, [contactTag])
 
 
   return (
-    <section className="App" >
-      <Navbar />
-      <div className="Content">
-        <section id="Techin" >
-          <PictureProfile />
-          <Sayhi />
-        </section>
-        <section id="Skills" ref={techinTag}><Skills /></section>
-        <section id="Contact"  ><Contact /></section>
-        <section id="Projects" ><MyProjects /></section>
-      </div>
-      <UnderBar />
-    </section>
+    <ConData.Provider value={{st_SkillTagTop, st_ContactTagTop, st_ProjectTagTop}}>
+      <section className="App" >
+        <Navbar />
+        <div className="Content">
+          <section id="Techin" >
+            <PictureProfile />
+            <Sayhi />
+          </section>
+          <section id="Skills" ref={skillTag} ><Skills /></section>
+          <section id="Contact" ref={contactTag} ><Contact /></section>
+          <section id="Projects" ref={projectTag} ><MyProjects /></section>
+        </div>
+        <UnderBar />
+      </section>
+    </ConData.Provider>
   );
 }
-
+export { ConData };
 export default App;
--- tk-webport/src/components/Navbar/NavBTN.js
+++ tk-webport/src/components/Navbar/NavBTN.js
@@ -1,13 +1,15 @@
+import { useContext } from 'react';
+import { ConData } from '../../App.js'
 import styles from './Navbar.module.css'
 // import React, { useState, useEffect } from 'react';
 
 function NavBTN() {
-    
+    const {st_SkillTagTop, st_ContactTagTop, st_ProjectTagTop} = useContext(ConData);
     return (
         <div className={styles.NavSpace}>
-            <a href="#Skills"><div className={styles.BTN}>SKILLS</div></a>
-            <a href="#Contact"><div className={styles.BTN}>CONTACT</div></a>
-            <a href="#Projects"><div className={styles.BTN}>PROJECTS</div></a>
+            <a href="#Skills"><div className={`${styles.BTN} ${st_SkillTagTop ? styles.HighLightBTN : ''}`}>SKILLS</div></a>
+            <a href="#Contact"><div className={`${styles.BTN} ${st_ContactTagTop ? styles.HighLightBTN : ''}`}>CONTACT</div></a>
+            <a href="#Projects"><div className={`${styles.BTN} ${st_ProjectTagTop ? styles.HighLightBTN : ''}`}>PROJECTS</div></a>
         </div>
     )
 }
