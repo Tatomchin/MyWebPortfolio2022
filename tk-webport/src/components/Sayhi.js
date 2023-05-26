af75b1c71cf8e819a33085d59cfc8c54dc508169
Tatomchin <tom.kowprasert@gmail.com>
2023-05-26 23:58:57 +0700

    Update for last boss ?????

--- tk-webport/src/components/Sayhi.js
+++ tk-webport/src/components/Sayhi.js
@@ -1,9 +1,14 @@
 import styles from './Sayhi.module.css'
+import animate from 'animate.css';
 
 function Sayhi() {
+    console.log(animate);
     return (
         <div className={styles.SayhiBg}>
-            <h1>Hello I'm <span id={styles.myName}>Techin Kowprasert</span> </h1>
+            <h1 className='animate__animated animate__tada animate__infinite animate__slower'>
+                Hello I'm
+            </h1>
+            <h2 id={styles.myName} > Techin Kowprasert</h2>
             <h2 id={styles.career}>Font-End & Game-Dev</h2>
             <h2 id={styles.welcome}>Welcome to my portfolio</h2>
             <p>
