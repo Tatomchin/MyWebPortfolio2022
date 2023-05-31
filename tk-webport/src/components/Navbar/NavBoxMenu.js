28da6197975fe7b9e64831a669df8a90e76b07db
Tatomchin <tom.kowprasert@gmail.com>
2023-05-31 14:03:12 +0700

    Update MenuHideBox

--- tk-webport/src/components/Navbar/NavBoxMenu.js
+++ tk-webport/src/components/Navbar/NavBoxMenu.js
@@ -14,12 +14,13 @@ function NavBoxMenu() {
                 <div className={styles.bar2}></div>
                 <div className={styles.bar3}></div>
             </div>
+            <div className={`${styles.bgCloseHiddeMenu} ${isActive ? '' : styles.showbgCloseHiddeMenu}`} onClick={toggleBoxMenu}/>
             <div className={`${styles.hideMenu} ${isActive ? '' : styles.showHiddenMenu}`}>
                 <ul>
                     <li><a href="#Techin">TECHIN</a></li>
                     <li><a href="#Skills">SKILLS</a></li>
-                    <li><a href="#Contact">CONTACT</a></li>
                     <li><a href="#Projects">PROJECTS</a></li>
+                    <li><a href="#Contact">CONTACT</a></li>
                 </ul>
             </div>
         </div>
